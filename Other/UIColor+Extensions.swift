4cefb0d017490966ff0782d1b6f1cfd25d225b02
dch000 <dmitriy.ev.ch@yandex.ru>
2023-05-28 11:28:46 +0300

    +

---
+++ Other/UIColor+Extensions.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+    static var YPBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.black}
+    static var YPBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue}
+    static var YPGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray}
+    static var YPRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red}
+    static var YPWhiteAlpha50: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white}
+    static var YPWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+}
