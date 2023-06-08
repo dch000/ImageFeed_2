76bf0c3d7c9f69c78115233a0c606d07842cfc20
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-06-08 22:01:04 +0300

    +

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet private var userpickImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+    @IBOutlet private var logoutButton: UIButton!
     
+    @IBAction func didTapLogoutButton() {
+    }
     
 }
