ac602fd6651c9d19499607fc23e40de20ad9ddeb
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-07-11 22:04:39 +0300

    +

--- ImageFeed/ProfileView/ProfileService.swift
+++ ImageFeed/ProfileView/ProfileService.swift
@@ -2,6 +2,29 @@ import UIKit
 
 final class ProfileService {
     
+    struct ProfileResult: Decodable {
+        let username: String?
+        let firstname: String?
+        let lastname: String?
+        let bio: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case username = "username"
+            case firstname = "first_name"
+            case lastname = "last_name"
+            case bio = "bio"
+        }
+    }
     
+    public struct Profile {
+        let username: String?
+        let name: String?
+        let loginName: String?
+        let bio: String?
+    }
+    
+    func fetchProfile(_ token: String, completion: @escaping (Result<Profile, Error>) -> Void){
+        
+    }
     
 }
