cd6d96b5112366e37aee7df7422c409b1b0d4fc2
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-07-29 10:12:23 +0300

    Update ImagesListModels.swift
    +

--- ImageFeed/ImagesList/ImagesListModels.swift
+++ ImageFeed/ImagesList/ImagesListModels.swift
@@ -9,3 +9,35 @@ struct Photo {
     let largeImageURL: String
     let isLiked: Bool
 }
+
+struct PhotosResult: Decodable {
+    let id: String
+    let createdAt: String?
+    let welcomeDescription: String?
+    let isLiked: Bool?
+    let width: Int?
+    let height: Int?
+    let urls: UrlsResult?
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case createdAt = "created_at"
+        case welcomeDescription = "description"
+        case isLiked = "liked_by_user"
+        case width = "width"
+        case height = "height"
+        case urls = "urls"
+    }
+}
+
+struct UrlsResult: Decodable {
+    let thumbImageURL: String?
+    let largeImageURL: String?
+    enum CodingKeys: String, CodingKey {
+        case thumbImageURL = "thumb"
+        case largeImageURL = "full"
+    }
+}
+
+struct IsLiked: Decodable {
+    let photo: PhotosResult?
+}
