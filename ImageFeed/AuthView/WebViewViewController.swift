97b41915909b7c68dcec6e416907d8d52752b4b0
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-08-14 21:11:41 +0300

    +

--- ImageFeed/AuthView/WebViewViewController.swift
+++ ImageFeed/AuthView/WebViewViewController.swift
@@ -84,19 +84,12 @@ extension WebViewViewController: WKNavigationDelegate {
                 decisionHandler(.allow)
             }
         }
-    /*
+    
     private func code(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == "/oauth/authorize/native",
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
+        if let url = navigationAction.request.url {
+            return presenter?.code(from: url)
         }
+        return nil
     }
-     */
+    
 }
