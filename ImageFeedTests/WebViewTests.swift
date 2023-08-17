94175dbe873593ec845e3b5278e392be62e66728
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-08-17 21:40:40 +0300

    tests

---
+++ ImageFeedTests/WebViewTests.swift
@@ -0,0 +1,22 @@
+
+@testable import ImageFeed
+import XCTest
+
+final class WebViewTests: XCTestCase {
+    
+    func testViewControllerCallsViewDidLoad() {
+        //given
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateViewController(withIdentifier: "WebViewViewController") as! WebViewViewController
+        let presenter = WebViewPresenterSpy()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        
+        //when
+        _ = viewController.view
+        
+        //then
+        XCTAssertTrue(presenter.viewDidLoadCalled) // behavior verification
+    }
+    
+}
