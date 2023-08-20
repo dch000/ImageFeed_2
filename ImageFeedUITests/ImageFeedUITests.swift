84145302ad4ab9d7d7666bfc23270db626fdad0e
Dmitriy <dmitriy.ev.ch@yandex.ru>
2023-08-20 20:19:17 +0300

    UI Tests

---
+++ ImageFeedUITests/ImageFeedUITests.swift
@@ -0,0 +1,85 @@
+import XCTest
+@testable import ImageFeed
+
+final class ImageFeedUITests: XCTestCase {
+    
+    private let app = XCUIApplication()
+    
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app.launch()
+    }
+    
+    func testAuth() throws {
+        app.buttons["Authenticate"].tap()
+        
+        let webView = app.webViews["UnsplashWebView"]
+        
+        XCTAssertTrue(webView.waitForExistence(timeout: 5))
+        let loginTextField = webView.descendants(matching: .textField).element
+        XCTAssertTrue(loginTextField.waitForExistence(timeout: 5))
+        
+        loginTextField.tap()
+        loginTextField.typeText("it@dcinc.ru")
+        loginTextField.swipeUp()
+        
+        let passwordTextField = webView.descendants(matching: .secureTextField).element
+        XCTAssertTrue(passwordTextField.waitForExistence(timeout: 5))
+        
+        passwordTextField.tap()
+        passwordTextField.typeText("prvt202399DCH")
+        loginTextField.swipeUp()
+        
+        app.buttons["login"].tap()
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        sleep(5)
+        print(app.debugDescription)
+    }
+    
+    func testFeed() throws {
+        // тестируем сценарий ленты
+        sleep(2)
+        
+        let tablesQuery = app.tables
+        let cell = tablesQuery.children(matching: .cell).element(boundBy: 0)
+        
+        sleep(2)
+        
+        cell.swipeUp()
+        
+        let cellToLike = tablesQuery.children(matching: .cell).element(boundBy: 1)
+        let likeButton = cellToLike.buttons["likeButton"]
+        
+        likeButton.tap()
+        
+        sleep(3)
+        
+        cellToLike.tap()
+        
+        sleep(2)
+        
+        let image = app.scrollViews.images.element(boundBy: 0)
+        image.pinch(withScale: 3, velocity: 1)
+        image.pinch(withScale: 0.5, velocity: -1)
+        
+        let navBackButtonWhiteButton = app.buttons["navBackButton"]
+        navBackButtonWhiteButton.tap()
+        
+    }
+    
+    func testProfile() throws {
+        // тестируем сценарий профиля
+        sleep(3)
+        app.tabBars.buttons.element(boundBy: 1).tap()
+        
+        XCTAssertTrue(app.staticTexts[""].exists)
+        XCTAssertTrue(app.staticTexts[""].exists)
+        
+        app.buttons["logoutButton"].tap()
+        
+        app.alerts["Выход"].scrollViews.otherElements.buttons["Да"].tap()
+    }
+}
